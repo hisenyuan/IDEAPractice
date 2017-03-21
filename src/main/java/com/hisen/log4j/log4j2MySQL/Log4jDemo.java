8c1677a4a30684bd0850a16d3d5b4e781d539424
hisenyuan <2811923@qq.com>
2017-03-21 14:46:30 +0800

    增加方法中打印日志到数据库，加上数据库输出信息

--- src/main/java/com/hisen/log4j/log4j2MySQL/Log4jDemo.java
+++ src/main/java/com/hisen/log4j/log4j2MySQL/Log4jDemo.java
@@ -9,11 +9,44 @@ import org.apache.log4j.Logger;
  */
 public class Log4jDemo {
     private static Logger logger = Logger.getLogger(Log4jDemo.class);
+
     public static void main(String[] args) {
         logger.debug("这是debug信息");
         // 记录info级别的信息
         logger.info("这是info信息");
         // 记录error级别的信息
         logger.error("这是error信息");
+        hisen();
     }
+
+    public static void hisen() {
+        logger.debug("这是方法中debug信息");
+        // 记录info级别的信息
+        logger.info("这是方法中info信息");
+        // 记录error级别的信息
+        logger.error("这是方法中error信息");
+    }
+/**
+ * mysql中的数据
+mysql> select * from log;
++---------------------------------------+--------+---------------------+----------+------------------------+
+| Class                                 | Mothod | CreateTime          | LogLevel | MSG                    |
++---------------------------------------+--------+---------------------+----------+------------------------+
+| com.hisen.log4j.Log4jDemo             | main   | 2017-03-21 12:49:10 | INFO     | This is info message1. |
+| com.hisen.log4j.Log4jDemo             | main   | 2017-03-21 12:49:11 | ERROR    | This is error message. |
+| com.hisen.log4j.Log4jDemo             | main   | 2017-03-21 12:54:34 | INFO     | This is info message1. |
+| com.hisen.log4j.Log4jDemo             | main   | 2017-03-21 12:54:34 | ERROR    | This is error message. |
+| com.hisen.log4j.log4jMySQL.Log4jDemo  | main   | 2017-03-21 13:36:27 | INFO     | 这是info信息           |
+| com.hisen.log4j.log4jMySQL.Log4jDemo  | main   | 2017-03-21 13:36:28 | ERROR    | 这是error信息          |
+| com.hisen.log4j.log4j2MySQL.Log4jDemo | main   | 2017-03-21 14:27:16 | INFO     | 这是info信息           |
+| com.hisen.log4j.log4j2MySQL.Log4jDemo | main   | 2017-03-21 14:27:16 | ERROR    | 这是error信息          |
+| com.hisen.log4j.log4j2MySQL.Log4jDemo | main   | 2017-03-21 14:39:34 | INFO     | 这是info信息           |
+| com.hisen.log4j.log4j2MySQL.Log4jDemo | main   | 2017-03-21 14:39:34 | ERROR    | 这是error信息          |
+| com.hisen.log4j.log4j2MySQL.Log4jDemo | main   | 2017-03-21 14:42:27 | INFO     | 这是info信息           |
+| com.hisen.log4j.log4j2MySQL.Log4jDemo | main   | 2017-03-21 14:42:27 | ERROR    | 这是error信息          |
+| com.hisen.log4j.log4j2MySQL.Log4jDemo | hisen  | 2017-03-21 14:42:27 | INFO     | 这是方法中info信息     |
+| com.hisen.log4j.log4j2MySQL.Log4jDemo | hisen  | 2017-03-21 14:42:27 | ERROR    | 这是方法中error信息    |
++---------------------------------------+--------+---------------------+----------+------------------------+
+14 rows in set
+*/
 }
