409bec2c0f964f9c7d6722fbbdaa57ff7d0d3f57
hisen <hisenyuan@gmail.com>
2017-03-25 12:17:45 +0800

    mybatis学习Dao开发之mapper代理方法

---
+++ src/main/java/com/hisen/mybatisStudy/no4/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package com.hisen.mybatisStudy.no4.mapper;
+
+import com.hisen.mybatisStudy.po.User;
+
+import java.util.List;
+
+/**
+ * Created by hisen on 17-3-25.
+ */
+public interface UserMapper {
+    //根据id查询用户信息
+    public User findUserById(int id) throws Exception;
+
+    //根据用户名列查询用户列表
+    public List<User> findUserByName(String name) throws Exception;
+
+    //添加用户信息
+    public void insertUser(User user) throws Exception;
+
+    //删除用户信息
+    public void deleteUser(int id) throws Exception;
+
+    //更新用户
+    public void updateUser(User user)throws Exception;
+}
---
+++ src/main/java/com/hisen/mybatisStudy/no4/mapper/UserMapperTest.java
@@ -0,0 +1,43 @@
+package com.hisen.mybatisStudy.no4.mapper;
+
+import com.hisen.mybatisStudy.po.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+/**
+ * Created by hisen on 17-3-25.
+ */
+public class UserMapperTest {
+    private SqlSessionFactory sqlSessionFactory;
+
+    //注解Before是在执行本类所有测试方法之前先调用这个方法
+    @Before
+    public void setup() throws Exception{
+        //创建SqlSessionFactory
+        String resource="SqlMapConfig.xml";
+        //将配置文件加载成流
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        //创建会话工厂，传入mybatis配置文件的信息
+        sqlSessionFactory=new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+    @Test
+    public void testFindUserById() throws Exception{
+
+        SqlSession sqlSession=sqlSessionFactory.openSession();
+
+        //创建UserMapper代理对象
+        UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+
+        //调用userMapper的方法
+        User user=userMapper.findUserById(1);
+
+        System.out.println(user.getUsername());
+    }
+}
