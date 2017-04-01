856703b07fa415d393f79babcde37d5798d1a76e
hisenyuan <2811923@qq.com>
2017-04-01 12:56:03 +0800

    测试fastjson

--- src/main/java/com/hisen/json/test.java
+++ src/main/java/com/hisen/json/test.java
@@ -4,12 +4,14 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 /**
+ * 测试fastjson
  * Created by hisenyuan on 2017/3/23 at 18:02.
  */
 public class test {
-    public static void main(String[] args) {
-        String s = "{'A':'a'}";
-        JSONObject obj= JSON.parseObject(s);
-        System.out.println(obj.get("A"));
-    }
+
+  public static void main(String[] args) {
+    String s = "{'A':'a'}";
+    JSONObject obj = JSON.parseObject(s);
+    System.out.println(obj.get("A"));
+  }
 }
