8189700b38616ef000cf200be4da9ccd9c0ec804
hisenyuan <2811923@qq.com>
2017-04-18 19:09:04 +0800

    文件 和 base64 相互转换

---
+++ src/main/java/com/hisen/String/File2Base64.java
@@ -0,0 +1,22 @@
+package com.hisen.String;
+
+import com.hisen.utils.Base64Util;
+import com.hisen.utils.File2ByteArraysUtil;
+
+/**
+ * Created by hisenyuan on 2017/4/18 at 18:13.
+ */
+public class File2Base64 {
+
+  public static void main(String[] args) {
+    byte[] bytes = File2ByteArraysUtil.file2Bytes("src/main/java/com/hisen/String/text/tomcat.png");
+    //图片加密为base64
+    String s = Base64Util.encodeBase64(bytes);
+    System.out.println("图片经Base64加密后的字符："+s);
+    //解密
+    byte[] s1 = Base64Util.decodeBase64(s);
+    //字符串转换回图片
+    File2ByteArraysUtil.bytes2File(s1,"src/main/java/com/hisen/String/text/build.png");
+  }
+
+}
