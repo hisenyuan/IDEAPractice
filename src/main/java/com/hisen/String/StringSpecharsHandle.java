66d8fb2e78ccf7516a9afaac30bb401ae2a73224
hisenyuan <hisenyuan@gmail.com>
2017-04-30 23:02:42 +0800

    特殊字符处理

---
+++ src/main/java/com/hisen/String/StringSpecharsHandle.java
@@ -0,0 +1,50 @@
+package com.hisen.String;
+
+public class StringSpecharsHandle {
+
+  public static void main(String[] args) {
+    String s = "\\大/家\\好\b我\f是\n李\r刚\t";
+    System.out.println(string2Json(s));
+  }
+
+  /**
+   * 特殊字符处理，比如：“\A1;1300”
+   *
+   * @return String
+   */
+  public static String string2Json(String s) {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < s.length(); i++) {
+      char c = s.charAt(i);
+      switch (c) {
+        case '\"':
+          sb.append("\\\"");
+          break;
+        case '\\':
+          sb.append("\\\\");
+          break;
+        case '/':
+          sb.append("\\/");
+          break;
+        case '\b':
+          sb.append("\\b");
+          break;
+        case '\f':
+          sb.append("\\f");
+          break;
+        case '\n':
+          sb.append("\\n");
+          break;
+        case '\r':
+          sb.append("\\r");
+          break;
+        case '\t':
+          sb.append("\\t");
+          break;
+        default:
+          sb.append(c);
+      }
+    }
+    return sb.toString();
+  }
+}
