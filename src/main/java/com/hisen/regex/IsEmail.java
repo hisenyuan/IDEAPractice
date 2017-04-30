5f1a8bdbf60af91320bd562dde517fc4533d256e
hisenyuan <hisenyuan@gmail.com>
2017-05-01 00:15:18 +0800

    正则表达式验证邮箱

--- src/main/java/com/hisen/regex/IsEmail.java
+++ src/main/java/com/hisen/regex/IsEmail.java
@@ -3,10 +3,13 @@ package com.hisen.regex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * 正则表达式验证邮箱
+ */
 public class IsEmail {
 
   public static void main(String[] args) {
-    String email = "hisenyuan@163.com";
+    String email = "hisenyuan@gmail.com";
     String patternstr = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
     Pattern pattern = Pattern.compile(patternstr);
     Matcher matcher = pattern.matcher(email);
