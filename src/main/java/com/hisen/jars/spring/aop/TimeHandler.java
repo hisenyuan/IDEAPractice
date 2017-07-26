6d8c117670938f1681ac06a0cb23af7e51c69987
hisenyuan <2811923@qq.com>
2017-07-26 17:03:12 +0800

    Spring-AOP 简单实现 之 JoinPoint

--- src/main/java/com/hisen/jars/spring/aop/TimeHandler.java
+++ src/main/java/com/hisen/jars/spring/aop/TimeHandler.java
@@ -1,5 +1,8 @@
 package com.hisen.jars.spring.aop;
 
+import java.util.Arrays;
+import org.aspectj.lang.JoinPoint;
+
 /**
  * 横切关注点 - 打印时间
  * Created by hisenyuan on 2017/7/26 at 13:46.
@@ -9,7 +12,15 @@ public class TimeHandler {
   /**
    * 打印时间
    */
-  public void printTime() {
+  public void printTime(JoinPoint joinPoint) {
     System.out.println("Spring AOP >>>>> CurrentTime = " + System.currentTimeMillis());
+    //Object[] getArgs：返回目标方法的参数
+    //Signature getSignature：返回目标方法的签名
+    //Object getTarget：返回被织入增强处理的目标对象
+    //Object getThis：返回AOP框架为目标对象生成的代理对象
+    System.out.println("执行过的函数相关信息 >>>>> 方法" + joinPoint.getSignature());
+    System.out.println("执行过的函数相关信息 >>>>> 参数" + Arrays.asList(joinPoint.getArgs()));
+    System.out.println("执行过的函数相关信息 >>>>> 目标" + joinPoint.getTarget());
+    System.out.println("执行过的函数相关信息 >>>>> 代理" + joinPoint.getThis());
   }
 }
