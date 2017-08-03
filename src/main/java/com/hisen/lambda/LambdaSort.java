3a6d06cc2528467203890baa25ba28cbc94d4752
hisenyuan <2811923@qq.com>
2017-08-03 14:27:15 +0800

    Lambda小小的尝试

---
+++ src/main/java/com/hisen/lambda/LambdaSort.java
@@ -0,0 +1,42 @@
+package com.hisen.lambda;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Created by hisenyuan on 2017/8/3 at 11:40.
+ */
+public class LambdaSort {
+
+  public static void main(String[] args) {
+    List<String> list = Arrays.asList("hisen", "test", "work", "name", "apple");
+    //利用lambda语法
+    Collections.sort(list, (o1, o2) -> o1.compareTo(o2));
+    for (String s : list) {
+      System.out.println(s);
+    }
+    System.out.println("--------");
+    //利用匿名内部类
+    Collections.sort(list, new Comparator<String>() {
+      @Override
+      public int compare(String o1, String o2) {
+        return o2.compareTo(o1);//反过来之后首字母倒序
+      }
+    });
+    for (String s : list) {
+      System.out.println(s);
+    }
+
+    System.out.println("--------");
+    //转换大小写
+    List<String> collect = list.stream().map((String name) -> {
+      return name.toUpperCase();
+    }).collect(Collectors.toList());
+    for (String s : collect){
+      System.out.println("转换为大写 >>>>> "+s);
+    }
+  }
+}
