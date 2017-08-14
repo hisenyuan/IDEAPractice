f008c4bff9d441f0817e1ff0f2f0db09ef3f322a
hisenyuan <hisenyuan@gmail.com>
2017-08-14 21:17:42 +0800

    计算机程序的思维逻辑 - 二进制/浮点数

--- src/main/java/com/hisen/programLogicOfThinking/binaryAndBitwisePart2/BinaryAndBitwise.java
+++ src/main/java/com/hisen/programLogicOfThinking/binaryAndBitwisePart2/BinaryAndBitwise.java
@@ -11,7 +11,7 @@ public class BinaryAndBitwise {
     System.out.println(Integer.toHexString(a));  //十六进制
 
     int b = 0x7B;//java不支持直接使用二进制
-    System.out.println("16进制：0x7B = "+b);
+    System.out.println("16进制：0x7B = " + b);
 
     /**
      * 位移运算
@@ -21,7 +21,7 @@ public class BinaryAndBitwise {
     int c = 8;
     int i = c >> 2;
     int i1 = c << 2;
-    System.out.printf("c >> 2 = %s\n,c << 2 = %s\n",i,i1);
+    System.out.printf("c >> 2 = %s\n,c << 2 = %s\n", i, i1);
 
     /**
      * 按位与 &：两位都为1才为1
@@ -31,7 +31,7 @@ public class BinaryAndBitwise {
      */
     boolean b1 = true;
     boolean b2 = false;
-    System.out.printf("\nb1 & b2 = %s\nb1 | b2 = %s\nb1 ^ b2 = %s\n",b1 & b2,b1 | b2,b1 ^ b2);
+    System.out.printf("\nb1 & b2 = %s\nb1 | b2 = %s\nb1 ^ b2 = %s\n", b1 & b2, b1 | b2, b1 ^ b2);
 
     /**
      * 可以精确表示为2的某次方之和的数可以精确表示，其他数则不能精确表示。
@@ -39,7 +39,18 @@ public class BinaryAndBitwise {
      * 2^(-2) = 0.25
      * 2^(-3) = 0.125
      * 2^(-4) = 0.0625
+     *
+     * 在误差足够小的时候，结果看上去是精确的，但不精确其实才是常态。
+     *
+     * 要精确可以先转换为整数，计算之后再转回去。
+     * 使用BigDecimal会更准确，但是效率低
      */
-
+    System.out.println("\n0.1f+0.1f = " + (0.1f + 0.1f));
+    System.out.println("0.1f*0.1f = " + 0.1f * 0.1f);
+    /**
+     * 小结
+     * 很多小数计算机中不能精确表示导致计算不准确
+     * 计算机的基本思维是二进制的
+     */
   }
 }
