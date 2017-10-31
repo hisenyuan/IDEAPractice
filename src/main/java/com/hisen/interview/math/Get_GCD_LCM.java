0c6331e60e466b0ca8c797f0114b6f43f59f036c
hisenyuan <2811923@qq.com>
2017-10-31 17:50:56 +0800

    求最大公约数和最小公倍数

--- src/main/java/com/hisen/interview/math/Get_GCD_LCM.java
+++ src/main/java/com/hisen/interview/math/Get_GCD_LCM.java
@@ -9,9 +9,12 @@ public class Get_GCD_LCM {
   public static void main(String[] args) {
     Get_GCD_LCM getGcdLcm = new Get_GCD_LCM();
     int gcd = getGcdLcm.getGCD(4, 8);
-    System.out.println("最大公约数：" + gcd);
-    int lcm = getGcdLcm.getLCM(4, 8);
-    System.out.println("最小公倍数：" + lcm);
+    System.out.println("递归方法，最大公约数：" + gcd + ",最小公倍数："+getGcdLcm.getLCM(4, 8));
+
+    int i = getGcdLcm.divisionGCD(4, 8);
+    System.out.println("\n辗转相除，最大公约数：" + i + ",最小公倍数：" + 4 * 8 / i);
+
+    System.out.println("相减法求最大公约数:"+getGcdLcm.subtractionGCD(4,8));
   }
 
   /**
@@ -26,12 +29,45 @@ public class Get_GCD_LCM {
 
   /**
    * 最小公倍数
-   * @param m
-   * @param n
-   * @return
    */
   public int getLCM(int m, int n) {
     int mn = m * n;
     return mn / getGCD(m, n);
   }
+
+  /**
+   * 辗转相除求最大公约数
+   * 有两整数a和b：
+   * ① a%b得余数c
+   * ② 若c=0，则b即为两数的最大公约数
+   * ③ 若c≠0，则a=b，b=c，再回去执行①
+   */
+  public int divisionGCD(int m, int n) {
+    int a;
+    while (n != 0) {
+      a = m % n;
+      m = n;
+      n = a;
+    }
+    return m;
+  }
+  /**
+   * 相减法求最大公约数
+   * 有两整数a和b：
+   * ① 若a>b，则a=a-b
+   * ② 若a<b，则b=b-a
+   * ③ 若a=b，则a（或b）即为两数的最大公约数
+   * ④ 若a≠b，则再回去执行①
+   */
+  public int subtractionGCD(int m,int n){
+    while(m != n){
+      if (m>n){
+        m = m-n;
+      }else {
+        n = n - m;
+      }
+    }
+    return m;
+  }
+
 }
