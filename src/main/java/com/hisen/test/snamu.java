86a60e00599d94035577618bf0d81bd731067239
hisenyuan <2811923@qq.com>
2017-11-07 17:13:11 +0800

    三目运算符

---
+++ src/main/java/com/hisen/test/snamu.java
@@ -0,0 +1,14 @@
+package com.hisen.test;
+
+/**
+ * @author : hisenyuan@gmail.com
+ * @date : 2017/10/22 17:14
+ */
+public class snamu {
+
+  public static void main(String[] args) {
+    String s = null;
+    String s1 = s == null || s.equals("") ? "1" : "2";
+    System.out.println(s1);
+  }
+}
