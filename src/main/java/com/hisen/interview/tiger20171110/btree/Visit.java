d1d2d9fd17795f766c1b1a2a4dd2812600f48ce3
hisenyuan <2811923@qq.com>
2017-11-13 13:21:14 +0800

    对节点进行操作的接口 - 二叉树的构造与遍历

--- src/main/java/com/hisen/interview/tiger20171110/btree/Visit.java
+++ src/main/java/com/hisen/interview/tiger20171110/btree/Visit.java
@@ -3,7 +3,7 @@ package com.hisen.interview.tiger20171110.btree;
 /**
  * @author : yhx
  * @date : 2017/11/13 12:14
- * @descriptor : 对节点进行操作的接口
+ * @descriptor : 对节点进行操作的接口 - 二叉树的构造与遍历
  */
 public interface Visit {
 
