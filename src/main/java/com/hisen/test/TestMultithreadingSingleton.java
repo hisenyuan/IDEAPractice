a35457830c10a7b8321ecc262b3a31ec56a5eac2
hisenyuan <2811923@qq.com>
2017-11-20 23:59:16 +0800

    多线程下的单例模式 - 改进

---
+++ src/main/java/com/hisen/test/TestMultithreadingSingleton.java
@@ -0,0 +1,13 @@
+package com.hisen.test;
+
+/**
+ * @author : yhx
+ * @date : 2017/11/16 21:42
+ * @descriptor :
+ */
+public class TestMultithreadingSingleton {
+
+  public static void main(String[] args) {
+
+  }
+}
