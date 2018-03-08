eb12090b622626c5a9f606bd02d9294db70c6669
hisenyuan <hisenyuan@gmail.com>
2018-03-08 11:52:56 +0800

    增加SHA-1加密算法工具类，修改多线程方法(增加CountDownLatch计数器)

--- src/main/java/com/hisen/jars/redisson/ResissonDistributedLockTest.java
+++ src/main/java/com/hisen/jars/redisson/ResissonDistributedLockTest.java
@@ -1,8 +1,17 @@
 package com.hisen.jars.redisson;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.hisen.utils.JedisUtil;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 
 /**
@@ -11,20 +20,45 @@ import redis.clients.jedis.Jedis;
  * @descriptor :
  */
 public class ResissonDistributedLockTest {
+  Logger LOGGER = LoggerFactory.getLogger(ResissonDistributedLockTest.class);
+
   private static Jedis jedis = JedisUtil.getJedis();
   private static RedissonClient client = RedissonUtil.getClient();
 
   @Test
   public void testThread(){
+    int num = 1000;
+    ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+        .setNameFormat("unpay-send-pool-%d").build();
+
+    CountDownLatch latch=new CountDownLatch(num);
+    // 创建线程池
+    ExecutorService executor = new ThreadPoolExecutor(
+        10,
+        10000,
+        200L,
+        TimeUnit.MILLISECONDS,
+        new LinkedBlockingQueue<Runnable>(5120),
+        namedThreadFactory,
+        new ThreadPoolExecutor.AbortPolicy());
+
+    // 业务逻辑
     jedis.set("yhx","0");
     System.out.println(jedis.get("yhx"));
     boolean locked = client.getLock("hisenyuan1").isLocked();
     System.out.println(locked);
-    for (int i = 0; i < 100000; i++) {
-      new MyThread(1,"t",client,jedis).start();
+    for (int i = 0; i < num; i++) {
+      executor.execute(new MyThread(1,"t",client,jedis,latch));
     }
-    locked = client.getLock("hisenyuan1").isLocked();
-    System.out.println(locked);
-    System.out.println(jedis.get("yhx"));
+    try {
+      latch.await();
+      LOGGER.info("已经停止所有的子线程");
+      executor.shutdown();
+      locked = client.getLock("hisenyuan1").isLocked();
+      System.out.println(locked);
+      System.out.println(jedis.get("yhx"));
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
---
+++ src/main/java/com/hisen/utils/SecureUtil.java
@@ -0,0 +1,95 @@
+package com.hisen.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import org.junit.Test;
+
+/**
+ * @author hisenyuan
+ * @time 2018/1/23 18:53
+ * @description 生成32位的MD5密码
+ */
+public class SecureUtil {
+
+  private static int HEX_FLAG = 0x0F;
+
+  @Test
+  public void md5Test() {
+    String text = "test";
+    String pwd = Md5(text);
+    System.out.println(pwd);
+  }
+
+  @Test
+  public void sha1Test() {
+    String test = "hisenyuan";
+    try {
+      System.out.println(getSHA1Str(test));
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * md5加密
+   */
+  private static String Md5(String unSafePwd) {
+    try {
+      MessageDigest md5 = MessageDigest.getInstance("MD5");
+      byte[] bytes = md5.digest(unSafePwd.getBytes("utf-8"));
+      return toHex(bytes);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  /**
+   * 从字节数组转换为16进制字符串
+   *
+   * @param md5Bytes md5加密之后的数据
+   */
+  private static String toHex(byte[] md5Bytes) {
+    final char[] HEX_DIGITS = "0123456789ABCDEF".toCharArray();
+    StringBuilder ret = new StringBuilder(md5Bytes.length * 2);
+    for (int i = 0; i < md5Bytes.length; i++) {
+      ret.append(HEX_DIGITS[(md5Bytes[i] >> 4) & HEX_FLAG]);
+      ret.append(HEX_DIGITS[md5Bytes[i] & HEX_FLAG]);
+    }
+    return ret.toString();
+  }
+
+  /**
+   * SHA-1加密算法
+   * @param dataStr
+   * @return
+   * @throws NoSuchAlgorithmException
+   * @throws UnsupportedEncodingException
+   */
+  public String getSHA1Str(String dataStr)
+      throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    MessageDigest md = MessageDigest.getInstance("SHA-1");
+    md.update(dataStr.getBytes("UTF-8"), 0, dataStr.length());
+    byte[] sha1hash = md.digest();
+    return convertToHex(sha1hash);
+  }
+
+  private String convertToHex(byte[] sha1Bytes) {
+    StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < sha1Bytes.length; ++i) {
+      int halfByte = (sha1Bytes[i] >>> 4) & 15;
+      int two_halfs = 0;
+      do {
+        if (halfByte >= 0 && halfByte <= 9) {
+          sb.append((char) (48 + halfByte));
+        } else {
+          sb.append((char) 97 + (halfByte - 10));
+        }
+        halfByte = sha1Bytes[i] & 15;
+      } while (two_halfs++ < 1);
+    }
+    return sb.toString();
+  }
+}
