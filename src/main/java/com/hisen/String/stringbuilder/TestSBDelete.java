36c0ebffdb15e37dc239f6e0600cff111eb41960
hisenyuan <hisenyuan@gmail.com>
2018-04-17 22:15:55 +0800

    循环拼接字符串，删除最后一个字符

---
+++ src/main/java/com/hisen/String/stringbuilder/TestSBDelete.java
@@ -0,0 +1,17 @@
+package com.hisen.String.stringbuilder;
+
+import org.junit.Test;
+
+/**
+ * @author hisenyuan
+ * @time 2018/4/17 22:11
+ * @description 删除Str最后一个字符
+ */
+public class TestSBDelete {
+    @Test
+    public void testDeleteCharAt(){
+        StringBuilder builder = new StringBuilder();
+        builder.append("a").append("=").append("b").append("&");
+        System.out.println(builder.deleteCharAt(builder.length()-1).toString());
+    }
+}
