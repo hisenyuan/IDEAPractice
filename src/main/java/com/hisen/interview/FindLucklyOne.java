1ead705bb621488bba8328d9d740e75adb38d748
hisenyuan <hisenyuan@gmail.com>
2018-07-20 17:46:19 +0800

    根据hashCode查找幸运儿

---
+++ src/main/java/com/hisen/interview/FindLucklyOne.java
@@ -0,0 +1,49 @@
+package com.hisen.interview;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * @author hisenyuan
+ * @time 2018/7/20 17:04
+ * @description 随机查找一个幸运儿 - 值班的人 23333
+ */
+public class FindLucklyOne {
+    public static void main(String[] args) {
+        List<String> person = Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H");
+        ArrayList<String> lucklys = new ArrayList<>();
+        for (int i = 0; i < person.size() * 50; i++) {
+            if (i % 8 == 0){
+                System.out.println("=================");
+            }
+            findLucklyOneByHashCode(person, lucklys);
+        }
+    }
+
+    private static void findLucklyOneByHashCode(List<String> person, ArrayList<String> lucklys) {
+        List<String> result = new ArrayList<>(person);
+        result.removeAll(lucklys);
+        if (result.size() == 0) {
+            lucklys.clear();
+            result = new ArrayList<>(person);
+        }
+        String code = System.nanoTime() + UUID.randomUUID().toString();
+        int index = Math.abs(code.hashCode()) % result.size();
+        String lucklyOne = result.get(index);
+        System.out.println("lucklyOne:" + lucklyOne);
+        lucklys.add(lucklyOne);
+    }
+    /**
+     * hashCode随机取出来的数据
+     * lucklyOne:G
+     * lucklyOne:B
+     * lucklyOne:C
+     * lucklyOne:D
+     * lucklyOne:H
+     * lucklyOne:F
+     * lucklyOne:E
+     * lucklyOne:A
+     */
+}
