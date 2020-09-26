d44f90a385688fdaf071035519854d5357286f8a
hisenyuan <hisenyuan@gmail.com>
2020-09-26 21:43:11 +0800

    支持单层 List 嵌套

--- src/main/java/com/hisen/copy/smaller/utils/CopyLimitedPropertiesUtil.java
+++ src/main/java/com/hisen/copy/smaller/utils/CopyLimitedPropertiesUtil.java
@@ -9,6 +9,8 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.PropertyAccessorFactory;
 
 import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -18,39 +20,73 @@ import java.util.List;
  * @date 2020-09-26 14:49
  */
 public class CopyLimitedPropertiesUtil {
+    /**
+     * all index
+     */
+    private final static String ALL_INDEX = "[-1]";
+
     public static void main(String[] args) {
         Order order = getOrder();
         Order smallerOrder = new Order();
         List<String> keepFields = Lists.newArrayList();
         keepFields.add("orderId");
         keepFields.add("address.province");
-        // i want get all names, not only index 1, how to do ?
-        keepFields.add("productInfos[1].name");
+        keepFields.add("productInfos" + ALL_INDEX + ".name");
+        keepFields.add("productInfos" + ALL_INDEX + ".imgUrl");
         copyLimitedProperties(order, smallerOrder, keepFields);
         Gson gson = new Gson();
         System.out.println("src:" + gson.toJson(order));
-        System.out.println("des" + gson.toJson(smallerOrder));
-//        src:{"address":{"province":"B","city":"A","county":"C"},"productInfos":[{"productId":0,"name":"name0","price":1,"num":0,"imgUrl":"url1"},{"productId":0,"name":"name1","price":2,"num":0,"imgUrl":"url1"},{"productId":0,"name":"name2","price":3,"num":0,"imgUrl":"url1"}]}
-//        des{"address":{"province":"B"},"productInfos":[{"productId":0,"num":0},{"productId":0,"name":"name1","num":0}]}
+        System.out.println("dst:" + gson.toJson(smallerOrder));
     }
 
 
     /**
-     * only copy fields you want
+     * only copy limited fields which you want
+     *
      * @param src
      * @param dst
      * @param fields
      */
-    private static void copyLimitedProperties(Order src, Order dst, List<String> fields) {
+    private static void copyLimitedProperties(Object src, Object dst, List<String> fields) {
         BeanWrapper srcWarp = PropertyAccessorFactory.forBeanPropertyAccess(src);
         BeanWrapper dstWarp = PropertyAccessorFactory.forBeanPropertyAccess(dst);
-        dstWarp.setAutoGrowCollectionLimit(10);
+        // 处理集合 key
+        handleCollection(fields, srcWarp);
         dstWarp.setAutoGrowNestedPaths(true);
         fields.forEach(f -> dstWarp.setPropertyValue(f, srcWarp.getPropertyValue(f)));
+
+    }
+
+    /**
+     * fill all index for collection key
+     *
+     * @param fields
+     * @param srcWarp
+     */
+    private static void handleCollection(List<String> fields, BeanWrapper srcWarp) {
+        Iterator<String> iterator = fields.iterator();
+        List<String> cl = Lists.newArrayList();
+        while (iterator.hasNext()) {
+            String field = iterator.next();
+            if (field.contains(ALL_INDEX)) {
+                int indexOf = field.indexOf(ALL_INDEX);
+                String key = field.substring(0, indexOf);
+                Object propertyValue = srcWarp.getPropertyValue(key);
+                if (propertyValue instanceof Collection) {
+                    Collection collection = (Collection) propertyValue;
+                    for (int i = 0; i < collection.size(); i++) {
+                        cl.add(key + "[" + i + "]" + field.substring(indexOf + ALL_INDEX.length()));
+                    }
+                    iterator.remove();
+                }
+            }
+        }
+        fields.addAll(cl);
     }
 
     /**
      * getOrder
+     *
      * @return
      */
     private static Order getOrder() {
@@ -64,6 +100,7 @@ public class CopyLimitedPropertiesUtil {
         int times = 3;
         for (int i = 0; i < times; i++) {
             ProductInfo productInfo = new ProductInfo();
+            productInfo.setProductId(i);
             productInfo.setName("name" + i);
             productInfo.setImgUrl("url" + 1);
             productInfo.setPrice(new BigDecimal(i + 1));
